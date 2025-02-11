556df2b7636879bf40f6f79b7ee572dda524616a
Vortex-Blade <damianlee526@gmail.com>
2025-02-11 15:01:51 +0100

    Fix abbout page text met padding en naam direct onder foto

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -15,13 +15,16 @@
     <RouterView/>
     </div>
   </nav>
-  <div class="Cardholder flex justify-evenly text-center bg-gray-100">
+  <div class="Cardholder flex justify-evenly text-left bg-gray-100">
     <div class="Card1 w-96 h-screen mt-6">
-      <div class="Card1inside bg-red-400 h-32 rounded-t-lg">
+      <div class="Card1inside bg-green-400 h-32 rounded-t-lg">
         <img src="../assets/Casper.jpg" alt="Profile Picture" class="rounded-full mt-6 mx-auto shadow-lg w-32 h-32 mb-8 object-cover"/>
       </div>
       <div class="bg-white rounded-b-lg h-96">
-          <p class="text-gray-600 text-lg mb-[100px]">
+          <div class="Name mx-[110px] mb-[10px]">
+            <big><strong>Casper Kizewski</strong></big>
+          </div>
+          <p class="text-gray-600 text-lg m-4 mb-[50px]">
           Casper Kizewski is student/programmeur, gedreven om innovatieve web-oplossingen te bouwen.
           Met expertise in Laravel en de TALL stack ontwikkelt hij moderne applicaties.
           Hij focust zich op het creëren van efficiënte en schaalbare software-oplossingen.
@@ -41,11 +44,14 @@
         <img src="../assets/Jochem.jpeg" alt="Profile Picture" class="rounded-full mt-6 mx-auto shadow-lg w-32 h-32 mb-8 object-cover"/>
       </div>
       <div class="bg-white rounded-b-lg h-96">
-          <p class="text-gray-600 text-lg mb-[100px]">
-            Jochem Mimpen is reli-ondernemer, geroepen om Gods koninkrijk verder te bouwen.
-            In samenwerking met anderen wil hij bestaande kerken activeren en nieuwe kerken planten.
-            Hij is getrouwd met Joanna en ze hebben drie zoons.
-          </p>
+        <div class="Name mx-[110px] mb-[10px]">
+          <big><strong>Jochem Mimpen</strong></big>
+        </div>
+        <p class="text-gray-600 text-lg m-4 mb-[50px]">
+          Jochem Mimpen is reli-ondernemer, geroepen om Gods koninkrijk verder te bouwen.
+          In samenwerking met anderen wil hij bestaande kerken activeren en nieuwe kerken planten.
+          Hij is getrouwd met Joanna en ze hebben drie zoons.
+        </p>
           <div class="flex justify-evenly">
             <a href="https://nl.linkedin.com/in/jochemmimpen" target="_blank">
             <img src="../assets/linkedin-logo.jpg" alt="LinkedIn" width="32" height="32">
